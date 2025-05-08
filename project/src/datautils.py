406db758cf1924e6435d5f3fabee97a643644ca6
deven367 <masterdeven@gmail.com>
2025-05-07 23:30:05 -0400

    dataloader for wikitext

--- project/src/datautils.py
+++ project/src/datautils.py
@@ -12,3 +12,28 @@ class MyTrainDataset(Dataset):
 
     def __getitem__(self, index):
         return self.data[index]
+
+class DatasetWithStride(Dataset):
+    """_summary_
+
+    Args:
+        Dataset (_type_): _description_
+    """
+
+    def __init__(self, data, block_size, stride):
+        self.data = data
+        self.block_size = block_size
+        self.stride = stride
+        self.num_samples = (len(data) - block_size) // stride
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        start_idx = idx * self.stride
+
+        chunk = self.data[start_idx : start_idx + self.block_size + 1]
+        x = chunk[:-1]
+        y = chunk[1:]
+        # print(idx, chunk.shape, x.shape, y.shape)
+        return x, y
