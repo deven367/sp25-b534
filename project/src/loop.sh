47ef11aa20800e4f156fa3c386f43d77801adb40
deven367 <masterdeven@gmail.com>
2025-05-08 19:58:24 -0400

    config for quartz

--- project/src/loop.sh
+++ project/src/loop.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-source /u/demistry/.venv/bin/activate
+# source /u/demistry/.venv/bin/activate
 
-for bsz in 64 128; do
-  for ctx in 512 1024 2048; do
+for bsz in 8 16 32 64 128; do
+  for ctx in 64 128 512 1024 2048; do
     # echo torchrun --nproc_per_node=2 ddp_train.py --batch_size $bsz --seq_len $ctx --epochs 1
     # torchrun --nproc_per_node=2 newtrainer.py --batch_size $bsz --seq_len $ctx --epochs 10
-    sbatch run.sh $bsz $ctx
+    # sbatch runq.sh $bsz $ctx
+    bash runq.sh $bsz $ctx
     echo "**************************************"
   done
 done
